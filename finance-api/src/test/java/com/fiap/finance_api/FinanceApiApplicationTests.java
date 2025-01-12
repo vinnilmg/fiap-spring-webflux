740f454ceb6e2ee2931cffd248fb0a1f2ca8bd47
= <vini.delimagomes@gmail.com>
2025-01-12 16:59:35 -0300

    feat: novo projeto

---
+++ finance-api/src/test/java/com/fiap/finance_api/FinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.finance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
